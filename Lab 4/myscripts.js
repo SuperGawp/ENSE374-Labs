a17b35d3bb2565d0d63c29bcfaa2821e36f429d3
SuperGawp <aryanchandra2002@hotmail.com>
2022-09-29 17:14:12 -0600

    change

--- Lab 4/myscripts.js
+++ Lab 4/myscripts.js
@@ -1,6 +1,12 @@
 function addItem()
 {
+    var text="";
     var input = document.getElementById("listText") = "";
+
+    for(var i = 0; i < input.length; i++)
+    {
+        text += input[i].value;
+    }
 
     div = document.createElement("div");
     div.className = "input-group mb-3";

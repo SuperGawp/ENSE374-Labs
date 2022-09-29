351b7dcd44e49922782c13ee7287cad085bc2395
SuperGawp <aryanchandra2002@hotmail.com>
2022-09-29 17:32:25 -0600

    change

--- Lab 4/myscripts.js
+++ Lab 4/myscripts.js
@@ -11,19 +11,22 @@ function addItem()
 function list()
 {
     document.getElementById("lists").innerHTML = "";
-    for(i = 0; i < listArray.length; i++)
+    if(input.length != 0)
     {
-        div = document.createElement("div");
-        div.className = "input-group mb-3";
-            
-        ans = document.createElement("input");
-        ans.Type = "text";
-        ans.className = "form-control";
-        ans.placeholder = listArray[i];
-        ans.setAttribute("disabled", listArray[i]);
+        for(i = 0; i < listArray.length; i++)
+        {
+            div = document.createElement("div");
+            div.className = "input-group mb-3";
+                
+            ans = document.createElement("input");
+            ans.Type = "text";
+            ans.className = "form-control";
+            ans.placeholder = listArray[i];
+            ans.setAttribute("disabled", listArray[i]);
 
-        div.appendChild(ans);
-        document.getElementById("lists").appendChild(div);
+            div.appendChild(ans);
+            document.getElementById("lists").appendChild(div);
+        }
     }
 }
 

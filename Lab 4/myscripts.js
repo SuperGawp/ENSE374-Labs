364296afd3587f0ef5fa318d5719ed4d11abe85f
SuperGawp <aryanchandra2002@hotmail.com>
2022-09-29 16:23:03 -0600

    change

--- Lab 4/myscripts.js
+++ Lab 4/myscripts.js
@@ -1,9 +1,9 @@
 function addItem()
 {
-    var li = document.createElement("LI");  
-    var input = document.getElementById("add");
+    var li = document.createElement("li");  
+    var input = document.getElementById("listText");
     li.innerHTML = input.value;
     input.value = "";
 
-    document.getElementById("faves").appendChild(li);
+    document.getElementById("list").appendChild(li);
 }

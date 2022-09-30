fff6471502167b9505a83af9a22d3d5ea3e08511
SuperGawp <aryanchandra2002@hotmail.com>
2022-09-29 18:04:20 -0600

    change

--- Lab 4/myscripts.js
+++ Lab 4/myscripts.js
@@ -1,37 +1,12 @@
-var listArray = [];
+function addItem() {
+    div = document.createElement('div');
+    div.className = "input-group mb-3";
+    
+    ans = document.createElement("input");
+    ans.Type = "text";
+    ans.className = "form-control";
 
-function addItem()
-{
-    input = document.getElementById("listText").value;
-    listArray.push(input);
-    document.getElementById("listText").value = "";
-    list();
-}
-
-function list()
-{
-    document.getElementById("lists").innerHTML = "";
-    //if(listArray.length != 0)
-    //{
-        for(i = 0; i < listArray.length; i++)
-        {
-            div = document.createElement("div");
-            div.className = "input-group mb-3";
-                
-            ans = document.createElement("input");
-            ans.Type = "text";
-            ans.className = "form-control";
-            ans.placeholder = listArray[i];
-            ans.setAttribute("disabled", listArray[i]);
-
-            div.appendChild(ans);
-            document.getElementById("lists").appendChild(div);
-        }
-    //}
-}
-
-function sortList()
-{
-    listArray.sort();
-    list();
-}
+    var inputVal = document.getElementById("listText").value;
+    div.appendChild(inputVal);
+    document.getElementById("lists").appendChild(div);
+  }

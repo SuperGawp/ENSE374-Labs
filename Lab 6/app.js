34da02df77b8464a9a27215f006b82a6d7a4f3db
SuperGawp <aryanchandra2002@hotmail.com>
2022-10-14 17:22:17 -0600

    change

--- Lab 6/app.js
+++ Lab 6/app.js
@@ -1,4 +1,18 @@
-const fs = require("fs");
+const fs = require( "fs" );
+fs.readFile ( __dirname + "/object.json",
+            "utf8", 
+            ( err, jsonString ) => {
+    if ( err ) {
+        console.log("Error reading file from disk:", err);
+        return;
+    }
+    try {
+        const object = JSON.parse(jsonString);
+        console.log("User's name is:", object.name); // Adam
+    } catch ( err ) {
+        console.log("Error parsing JSON:", err);
+    }
+});
 
 let s = "hello world"
 

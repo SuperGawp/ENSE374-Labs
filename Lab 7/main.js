c0752d218bb15b1ad053e0e27bf5c9771be1f90d
SuperGawp <aryanchandra2002@hotmail.com>
2022-10-27 20:28:10 -0600

    change

--- Lab 7/main.js
+++ Lab 7/main.js
@@ -26,6 +26,7 @@ app.use(express.static("public"))
 
 app.use(bodyParser.json());
 
+
 app.post("/login", (req, res) => {
     var loginEmail = req.body["email1"];
     var loginPassword = req.body["psw1"];
@@ -71,52 +72,60 @@ app.post("/login", (req, res) => {
     });
 });
 
+function getUsers()
+{
+    var fs = require('fs')
+    fs.writeFile('./public/users.json', JSON.stringify(usersLoaded), (err) => {
+        if (err) console.log('Error writing file:', err);
+    });
+}
+
+function addUsers()
+{
+    usersLoaded = require('./public/users.json');
+}
+
+function getTasks()
+{
+    var fs = require('fs')
+    fs.writeFile('./public/task.json', JSON.stringify(tasksLoaded), (err) => {
+        if (err) console.log('Error writing file:', err);
+    });
+}
+
+function addTasks()
+{
+    tasksLoaded = require('/public/task.json');
+}
+
 app.post("/register", (req, res) => {
+    addUsers();
+    var isItTrue = 0;
     var registerEmail = req.body["email2"];
     var registerPassword = req.body["psw2"];
     var registerAuth = req.body["auth"];
 
-    const fs = require( "fs" );
-    fs.readFile ( __dirname + "/public/users.json",
-            "utf8", 
-            ( err, jsonString ) => {
-    if ( err ) 
+    if(registerAuth == "todo2022")
     {
-        console.log("Error reading file from disk:", err);
-        return;
-    }
-    try 
-    {
-        const users = JSON.parse(jsonString);
-
-        console.log(users); // entire users.json
-
-        for(var i = 0; i < registerEmail.length; i++)
+        for(var i = 0; i < usersLoaded.length; i++)
         {
-            console.log("registerEmail: " + registerEmail);
-            console.log("registerPassword: " + registerPassword);
-            console.log("registerAuth: " + registerAuth);
-            console.log("TrueEmail: " + users[i].username);
-            console.log("TruePassword: " + users[i].password);
-    
-            if(registerEmail != users[i].username && registerAuth === 'todo2022')
-            {   
-                var more = {"username": registerEmail, "password": registerPassword};
-                users.push(more);
-
-                console.log("SUCCESS");
-                res.redirect("/todo.html");
-            }
-            else
+            if(registerEmail == usersLoaded[i].username)
             {
-                console.log("FAILURE");
+                isItTrue = 1;
                 res.redirect("/");
+                break;
             }
         }
+        if(isItTrue != 1)
+        {
+            usersLoaded.push({"username": registerEmail, "password": registerPassword});
+            getUsers();
+            res.redirect("/todo.html");
+        }
+        isItTrue = 0;
     }
-    catch ( err ) 
+    else
     {
-        console.log("Error parsing JSON:", err);
+        res.redirect("/");
     }
-    });
 });
